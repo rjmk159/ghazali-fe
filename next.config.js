d1df826aa13e8e7a43083bc009a5515c63ac8201
Raja Khan <proservz@Proservz-DELSpare-Pro1s-MacBook-Pro.local>
2022-10-12 02:34:44 +0530

    Changing attempts

--- next.config.js
+++ next.config.js
@@ -15,4 +15,4 @@ module.exports = withStoreConfig({
   },
 })
 
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+console.log("next.config.js", JSON.stringify(module.exports, null, 10))
