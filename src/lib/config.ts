dfd6d5e0f61d27a844c80e7b0f6dc9058b97f586
Raja Khan <proservz@Proservz-DELSpare-Pro1s-MacBook-Pro.local>
2022-10-12 02:12:36 +0530

    heroku

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,8 @@ import { QueryClient } from "react-query"
 // Defaults to standard port for Medusa server
 let MEDUSA_BACKEND_URL = "https://ghazali-server.herokuapp.com/"
 
+
+
 // if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
 //   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 // }
