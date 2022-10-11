f1a163eded6a4f61cc5f1209921eba8fc2533d75
Raja Khan <proservz@Proservz-DELSpare-Pro1s-MacBook-Pro.local>
2022-10-11 21:13:15 +0530

    new changes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,11 +2,11 @@ import Medusa from "@medusajs/medusa-js"
 import { QueryClient } from "react-query"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "https://ghazali-server-x2nb.onrender.com"
 
-if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
-}
+// if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
+//   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
+// }
 
 const queryClient = new QueryClient({
   defaultOptions: {
